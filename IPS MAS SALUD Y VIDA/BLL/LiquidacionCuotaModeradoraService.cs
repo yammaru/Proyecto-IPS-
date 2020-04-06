13ecb07db41d5c844993adcff16e470d0912dd37
yammaru <63098405+yammaru@users.noreply.github.com>
2020-04-06 08:34:09 -0500

    Add files via upload

---
+++ IPS MAS SALUD Y VIDA/BLL/LiquidacionCuotaModeradoraService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entity;
+
+namespace BLL
+
+{
+    public class LiquidacionCuotaModeradoraService
+    {
+        LiquidacionCuataMoeradoraRepository RepositorioLiquidacion = new LiquidacionCuataMoeradoraRepository();
+        public string Guardar(LiquidacionCuotaModeradora liquidacion)
+        {
+            if (RepositorioLiquidacion.Buscar(liquidacion.NumeroLiquidacion)==null)
+            {
+                RepositorioLiquidacion.Guardar(liquidacion);
+                return $"la liquidacion fue guarda";
+            }
+            return $"paciente {liquidacion.NumeroLiquidacion}, ya exite";
+        }
+       
+        public string Eliminar(string numeroLiquidacion)
+        {
+            LiquidacionCuotaModeradora liquidacion = RepositorioLiquidacion.Buscar(numeroLiquidacion);
+            if (liquidacion == null)
+            {
+                return $"la liquidacion {numeroLiquidacion}, no exite";
+            }
+            RepositorioLiquidacion.Eliminar(liquidacion);
+            return $"la liquidacion {liquidacion.NumeroLiquidacion} fue borrada";
+        }
+        public List<LiquidacionCuotaModeradora> Consultar() { return RepositorioLiquidacion.Consultar(); }
+        public LiquidacionCuotaModeradora Buscar(string id){return RepositorioLiquidacion.Buscar(id);}
+    }
+}
